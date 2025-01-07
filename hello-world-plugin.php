7bb9899935d11837af3fd0f68875d3ca0f8362e1
Pancakezys <Sandriskovaljonoks09@gmail.com>
2025-01-07 13:07:03 +0200

    plugin fix

--- hello-world-plugin.php
+++ hello-world-plugin.php
@@ -1,12 +1,21 @@
 <?php
+/*
+Plugin Name: Add Text To Footer
+Version: 1.0
+*/
+
 namespace Pancakezys\HelloWorldPlugin;
 
-class Plugin {
-    public function __construct() {
+class Plugin
+{
+    public function __construct()
+    {
         add_action('wp_footer', [$this, 'hello_world']);
     }
 
-    public function hello_world() {
+    public function hello_world()
+    {
         echo "<p>Hello World from Pancakezys Plugin!</p>";
     }
 }
+new Plugin();
